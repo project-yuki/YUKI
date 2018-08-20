62ac157f0d272c256e7ba67cca0e0eb88ff8ac68
tinyAdapter <tinyadapter@outlook.com>
2018-08-20 19:25:55 +0800

    install vue-devtools

--- src/main/index.dev.js
+++ src/main/index.dev.js
@@ -15,12 +15,13 @@ require('electron-debug')({ showDevTools: false })
 
 // Install `vue-devtools`
 require('electron').app.on('ready', () => {
-  let installExtension = require('electron-devtools-installer')
-  installExtension.default(installExtension.VUEJS_DEVTOOLS)
-    .then(() => {})
-    .catch(err => {
-      console.log('Unable to install `vue-devtools`: \n', err)
-    })
+  // let installExtension = require('electron-devtools-installer')
+  // installExtension.default(installExtension.VUEJS_DEVTOOLS)
+  //   .then(() => {})
+  //   .catch(err => {
+  //     console.log('Unable to install `vue-devtools`: \n', err)
+  //   })
+  require('vue-devtools').install()
 })
 
 // Require `main` process to boot app
