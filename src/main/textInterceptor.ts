1c7b56ac076ea3e72db5e9967f2e3bbaabbde73d
tinyAdapter <tinyadapter@outlook.com>
2018-10-01 21:36:39 +0800

    add text length judgement

--- src/main/textInterceptor.ts
+++ src/main/textInterceptor.ts
@@ -5,13 +5,22 @@ export default class TextInterceptor {
   }
 
   senselessTextPatterns: string[] = ["windowbtn", "00_プロローグ１", "value"];
+  static readonly MAX_LENGTH = 255;
 
   isSenseless(text: string): boolean {
+    return this.isTooLong(text) || this.containsSenselessText(text);
+  }
+
+  private containsSenselessText(text: string) {
     for (let pattern of this.senselessTextPatterns) {
       if (text.indexOf(pattern) > -1) {
         return true;
       }
     }
     return false;
+  }
+
+  private isTooLong(text: string) {
+    return text.length > TextInterceptor.MAX_LENGTH;
   }
 }
