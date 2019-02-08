e2b44e8aec68bc1ab0c327822f4bb0ae84cefa93
tinyAdapter <tinyadapter@outlook.com>
2019-02-08 11:43:20 +0800

    bugfix: JSON request body

--- src/main/translate/api.ts
+++ src/main/translate/api.ts
@@ -1,21 +1,21 @@
 const request = require("request");
 import { Options, Response } from "request";
 import logger from "../../common/logger";
+import * as vm from "vm";
 
 export default class Api implements Yagt.Translator {
   private config: Yagt.Config.OnlineApiItem;
-  private requestOptions: Options;
+  private requestOptions: Options = {
+    url: this.config.url,
+    method: this.config.method,
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0"
+    }
+  };
 
   constructor(config: Yagt.Config.OnlineApiItem) {
     this.config = config;
-    this.requestOptions = {
-      url: this.config.url,
-      method: this.config.method,
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0"
-      }
-    };
   }
 
   async translate(text: string) {
@@ -37,7 +37,7 @@ export default class Api implements Yagt.Translator {
     if (this.config.requestBodyFormat.startsWith("X")) {
       this.requestOptions.form = JSON.parse(requestBodyString.substring(1));
     } else if (this.config.requestBodyFormat.startsWith("J")) {
-      this.requestOptions.json = eval(requestBodyString.substring(1));
+      this.requestOptions.json = JSON.parse(requestBodyString.substring(1));
     } else {
       logger.error(`API [${this.config.name}]: No such request body type`);
     }
