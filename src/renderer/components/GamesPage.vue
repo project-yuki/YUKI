4211204491e3e5163e76a60af84a797f8bf72645
tinyAdapter <tinyadapter@outlook.com>
2019-02-13 22:22:29 +0800

    add prompt when there are no games
    ヾ(•ω•`)o

--- src/renderer/components/GamesPage.vue
+++ src/renderer/components/GamesPage.vue
@@ -2,6 +2,9 @@
   <div>
     <gt-page-header title="我的游戏"/>
     <gt-page-content>
+      <p v-if="noGame" class="all-center text-h3">什么都没有呢(っ °Д °;)っ
+        <br>快去添加游戏吧~ヾ(•ω•`)o
+      </p>
       <mu-row gutter>
         <mu-col sm="12" md="6" lg="4" v-for="game in games" :key="game.path">
           <gt-game-card :game="game"></gt-game-card>
@@ -29,8 +32,18 @@ import GtGameCard from "@/components/GamesPageGameCard.vue";
 })
 export default class GamesPage extends Vue {
   @namespace("Config").State("games") games!: Yagt.Game[];
+
+  get noGame() {
+    return this.games.length === 0;
+  }
 }
 </script>
 
-<style>
+<style scope>
+.all-center {
+  position: absolute;
+  top: 48%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
 </style>
