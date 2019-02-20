3c62a75f89327fbcabf48540aa8761930c6e4e41
tinyAdapter <tinyadapter@outlook.com>
2019-02-20 22:43:44 +0800

    update youdao api
    Maybe not stable, still need further test

--- config/youdaoApi.js
+++ config/youdaoApi.js
@@ -2,17 +2,18 @@ SESSION_URL = "http://fanyi.youdao.com";
 TRANSLATE_URL =
   "http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule";
 USER_AGENT =
-  "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0";
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36";
 
 var session;
 var bv;
 var requestTranslation;
+var initSession;
 
 if (!session) {
   session = Request.jar();
 
   requestTranslation = () => {
-    let p = new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       let ts = "" + new Date().getTime();
       let salt = ts + parseInt(10 * Math.random(), 10);
       let sign = md5(
@@ -38,7 +39,7 @@ if (!session) {
           doctype: "json",
           version: "2.1",
           keyfrom: "fanyi.web",
-          action: "FY_BY_CLICKBUTTION",
+          action: "FY_BY_REALTIME",
           typoResult: false,
           sign: sign,
           bv: bv
@@ -53,26 +54,29 @@ if (!session) {
           resolve(result);
         })
         .catch(err => {
-          reject(err);
+          return initSession();
         });
     });
-    return p;
   };
 
-  result = Request.get(SESSION_URL, {
-    jar: session,
-    gzip: true,
-    headers: {
-      Referer: SESSION_URL,
-      "User-Agent": USER_AGENT
-    }
-  })
-    .then(() => {
-      bv = md5("5.0 (Windows)", "hex");
-      let ts = "" + new Date().getTime();
-      session.setCookie(Request.cookie(`___rl__test__cookies=${ts - 10}`));
+  initSession = () => {
+    return Request.get(SESSION_URL, {
+      jar: session,
+      gzip: true,
+      headers: {
+        Referer: SESSION_URL,
+        "User-Agent": USER_AGENT
+      }
     })
-    .then(requestTranslation);
+      .then(() => {
+        bv = md5("5.0 (Windows)", "hex");
+        let ts = "" + new Date().getTime();
+        session.setCookie(Request.cookie(`___rl__test__cookies=${ts - 10}`));
+      })
+      .then(requestTranslation);
+  };
+
+  result = initSession();
 } else {
   result = requestTranslation();
 }
