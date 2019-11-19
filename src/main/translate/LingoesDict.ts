7cd494bfed0d1a578ac91e72e6ee60e0d5ce048d
tinyAdapter <tinyadapter@outlook.com>
2019-11-19 14:56:01 +0800

    bugfix: ignore exception when parse errors

--- src/main/translate/LingoesDict.ts
+++ src/main/translate/LingoesDict.ts
@@ -8,106 +8,110 @@ export default class LingoesDict {
     content: string,
     callback: (pattern?: yuki.LingoesPattern) => void
   ) {
-    xml2js.parseString(content, (err, result: any) => {
-      if (err) callback()
+    try {
+      xml2js.parseString(content, (err, result: any) => {
+        if (err) callback()
 
-      const refinedResult: yuki.LingoesPattern = {}
-      let parseResult: any = { ...result }
-      if (!parseResult.C) {
-        callback()
-        return
-      }
-      parseResult = parseResult.C
-      if (!parseResult.F) {
-        callback()
-        return
-      }
-      parseResult = parseResult.F
-      if (!parseResult[0]) {
-        callback()
-        return
-      }
-      parseResult = parseResult[0]
-      if (parseResult.H) {// parse kana
-        let kanaResult = parseResult.H
-        if (kanaResult[0]) {
-          kanaResult = kanaResult[0]
-          if (kanaResult.M) {
-            kanaResult = kanaResult.M
-          }
-          refinedResult.kana = kanaResult
+        const refinedResult: yuki.LingoesPattern = {}
+        let parseResult: any = { ...result }
+        if (!parseResult.C) {
+          callback()
+          return
         }
-      }
-
-      if (!parseResult.I) {
-        callback(refinedResult)
-        return
-      }
-      parseResult = parseResult.I
-      if (!parseResult[0]) {
-        callback(refinedResult)
-        return
-      }
-      parseResult = parseResult[0]
-      if (!parseResult.N) {
-        callback(refinedResult)
-        return
-      }
-      parseResult = parseResult.N
-      if (parseResult[0]) {// parse parts of speech and explanations
-        refinedResult.definitions = []
-        for (let definitionResult of parseResult) {
-          const oneDefinition: any = {}
-          if (!definitionResult.P) {
-            oneDefinition.partOfSpeech = undefined
-          } else {
-            oneDefinition.partOfSpeech = definitionResult.P[0].U[0]
-          }
-          if (!definitionResult.Q) {
-            oneDefinition.explanations = undefined
-          } else {
-            oneDefinition.explanations = []
-            definitionResult = definitionResult.Q
-            let oneExplanation = {
-              content: '',
-              example: {
-                sentence: '',
-                content: ''
-              }
+        parseResult = parseResult.C
+        if (!parseResult.F) {
+          callback()
+          return
+        }
+        parseResult = parseResult.F
+        if (!parseResult[0]) {
+          callback()
+          return
+        }
+        parseResult = parseResult[0]
+        if (parseResult.H) {// parse kana
+          let kanaResult = parseResult.H
+          if (kanaResult[0]) {
+            kanaResult = kanaResult[0]
+            if (kanaResult.M) {
+              kanaResult = kanaResult.M
             }
-            for (const iStr in definitionResult) {
-              const i = Number.parseInt(iStr, 10)
-              if (typeof definitionResult[i] === 'string') {
-                // appendix of the last explanation
-                oneExplanation.content += definitionResult[i]
-              } else if (typeof definitionResult[i] === 'object') {
-                // end of the last explanation, with potential example
-                oneExplanation.content += definitionResult[i]._
-                oneExplanation.example.sentence = definitionResult[i].T[0].W[0]
-                oneExplanation.example.content = definitionResult[i].T[0].X[0]
-                oneDefinition.explanations.push(oneExplanation)
-                oneExplanation = {
-                  content: '',
-                  example: {
-                    sentence: '',
-                    content: ''
-                  }
+            refinedResult.kana = kanaResult
+          }
+        }
+
+        if (!parseResult.I) {
+          callback(refinedResult)
+          return
+        }
+        parseResult = parseResult.I
+        if (!parseResult[0]) {
+          callback(refinedResult)
+          return
+        }
+        parseResult = parseResult[0]
+        if (!parseResult.N) {
+          callback(refinedResult)
+          return
+        }
+        parseResult = parseResult.N
+        if (parseResult[0]) {// parse parts of speech and explanations
+          refinedResult.definitions = []
+          for (let definitionResult of parseResult) {
+            const oneDefinition: any = {}
+            if (!definitionResult.P) {
+              oneDefinition.partOfSpeech = undefined
+            } else {
+              oneDefinition.partOfSpeech = definitionResult.P[0].U[0]
+            }
+            if (!definitionResult.Q) {
+              oneDefinition.explanations = undefined
+            } else {
+              oneDefinition.explanations = []
+              definitionResult = definitionResult.Q
+              let oneExplanation = {
+                content: '',
+                example: {
+                  sentence: '',
+                  content: ''
                 }
-              } else {
-                // Error, stop parsing
-                break
+              }
+              for (const iStr in definitionResult) {
+                const i = Number.parseInt(iStr, 10)
+                if (typeof definitionResult[i] === 'string') {
+                  // appendix of the last explanation
+                  oneExplanation.content += definitionResult[i]
+                } else if (typeof definitionResult[i] === 'object') {
+                  // end of the last explanation, with potential example
+                  oneExplanation.content += definitionResult[i]._
+                  oneExplanation.example.sentence = definitionResult[i].T[0].W[0]
+                  oneExplanation.example.content = definitionResult[i].T[0].X[0]
+                  oneDefinition.explanations.push(oneExplanation)
+                  oneExplanation = {
+                    content: '',
+                    example: {
+                      sentence: '',
+                      content: ''
+                    }
+                  }
+                } else {
+                  // Error, stop parsing
+                  break
+                }
+              }
+              if (typeof definitionResult[definitionResult.length - 1] === 'string') {
+                oneDefinition.explanations.push(oneExplanation)
               }
             }
-            if (typeof definitionResult[definitionResult.length - 1] === 'string') {
-              oneDefinition.explanations.push(oneExplanation)
-            }
+            refinedResult.definitions.push(oneDefinition)
           }
-          refinedResult.definitions.push(oneDefinition)
         }
-      }
 
-      callback(refinedResult)
-    })
+        callback(refinedResult)
+      })
+    } catch (e) {
+      debug('parse XML error !> %s', e)
+    }
   }
 
   private config: yuki.Config.LibraryItem
